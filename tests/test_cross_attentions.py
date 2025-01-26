8ac61c418821858625334c6bb829347188c9d322
Luke Thompson <luke-a-thompson@outlook.com>
2025-01-26 23:36:09 +1100

    Feat: Tests

---
+++ tests/test_cross_attentions.py
@@ -0,0 +1,200 @@
+import torch
+from gtno_py.gtno.cross_attentions import TemporalRoPEWithOffset
+
+
+class TestTemporalRoPEWithOffsetGroup:
+    def test_temporal_rope_vector_norm_unchanged(self):
+        batch_size = 2
+        num_timesteps = 2
+        num_nodes = 3
+        d = 4
+        num_heads = 2
+        d_head = d // num_heads
+
+        # Batches = blue; heads = yellow; nodes * timesteps = purple
+        # [Batch, heads, nodes * timesteps, d_head]
+        flattened_x = torch.tensor(
+            [
+                [
+                    [
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                    ],
+                    [
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                    ],
+                ],
+                [
+                    [
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                    ],
+                    [
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                    ],
+                ],
+            ]
+        )
+        # Freqs =  1.0, 0.0316227766016838
+        rope_output = TemporalRoPEWithOffset(num_timesteps=num_timesteps, d_head=d_head, n_heads=num_heads).forward(flattened_x)
+
+        # [Batch, heads, nodes * timesteps, d_head]
+        # Purple = [even, odd]
+        expected_rope_output = torch.tensor(
+            [
+                [
+                    [
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [-0.3011, 1.3817],
+                        [-0.3011, 1.3817],
+                        [-0.3011, 1.3817],
+                    ],
+                    [
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [-0.3011, 1.3817],
+                        [-0.3011, 1.3817],
+                        [-0.3011, 1.3817],
+                    ],
+                ],
+                [
+                    [
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [-0.3011, 1.3817],
+                        [-0.3011, 1.3817],
+                        [-0.3011, 1.3817],
+                    ],
+                    [
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [-0.3011, 1.3817],
+                        [-0.3011, 1.3817],
+                        [-0.3011, 1.3817],
+                    ],
+                ],
+            ],
+            dtype=torch.float32,
+        )
+
+        assert rope_output.shape == expected_rope_output.shape
+        assert torch.allclose(rope_output, expected_rope_output, atol=1e-2), f"rope_output: \n{rope_output}, \nexpected_rope_output: \n{expected_rope_output}"
+
+    def test_rope_timestep_interleave(self):
+        num_timesteps = 3
+        num_nodes = 4
+
+        times = torch.arange(num_timesteps).unsqueeze(1)  # [T,1]
+        positions = torch.repeat_interleave(times, num_nodes, dim=1).flatten(0, 1)  # [N*T=seq_len]
+
+        assert torch.equal(positions, torch.tensor([0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2]))
+
+    def test_rope_stack_interleave(self):
+        original = torch.tensor([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12]], dtype=torch.int32)
+
+        t1 = original[..., 0::2]
+        t2 = original[..., 1::2]
+
+        new = torch.stack([t1, t2], dim=-1).view_as(original)
+
+        assert torch.equal(new, original)
+
+    def test_rope_cos_sin_rotations(self):
+        num_timesteps = 2
+        num_nodes = 3
+        num_heads = 2
+        d_head = 4
+
+        times = torch.arange(num_timesteps).unsqueeze(1)  # [T,1]
+        positions = torch.repeat_interleave(times, num_nodes, dim=1).flatten(0, 1)  # [N*T=seq_len]
+
+        offset = torch.zeros(num_heads)
+        freqs = torch.tensor([[[1.0, 0.0316227766016838]]])  # Analytically derived from d_head = 4, manually unsqueezed
+
+        positions_broadcast = positions.unsqueeze(0)  # [1, seq_len]
+        offset_broadcast = offset.unsqueeze(-1)  # [H, 1]
+        shifted_positions = positions_broadcast + offset_broadcast
+
+        angle = shifted_positions.unsqueeze(-1) * freqs
+
+        assert angle.shape == torch.Size([num_heads, num_nodes * num_timesteps, d_head // 2])
+
+        cos_t = angle.cos().unsqueeze(0)
+        sin_t = angle.sin().unsqueeze(0)
+
+        # [1, H, seq_len, half_dim]
+        # Using cosine of freqs
+        # Head = yellow
+        expected_cos_t = torch.tensor(
+            [
+                [
+                    [
+                        [1, 1],  # T = 0; left = timestep, right = timestep * freq
+                        [1, 1],  # T = 0
+                        [1, 1],  # T = 0
+                        [0.5403, 0.9995],  # T = 1
+                        [0.5403, 0.9995],  # T = 1
+                        [0.5403, 0.9995],  # T = 1
+                    ],
+                    [
+                        [1, 1],
+                        [1, 1],
+                        [1, 1],
+                        [0.5403, 0.9995],
+                        [0.5403, 0.9995],
+                        [0.5403, 0.9995],
+                    ],
+                ]
+            ],
+            dtype=torch.float32,
+        )
+
+        expected_sin_t = torch.tensor(
+            [
+                [
+                    [
+                        [0, 0],  # T = 0; left = timestep, right = timestep * freq
+                        [0, 0],  # T = 0
+                        [0, 0],  # T = 0
+                        [0.8414, 0.0316],  # T = 1
+                        [0.8414, 0.0316],  # T = 1
+                        [0.8414, 0.0316],  # T = 1
+                    ],
+                    [
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                        [0.8414, 0.0316],
+                        [0.8414, 0.0316],
+                        [0.8414, 0.0316],
+                    ],
+                ]
+            ],
+            dtype=torch.float32,
+        )
+
+        assert torch.allclose(cos_t, expected_cos_t, atol=1e-3), f"cos_t: \n{cos_t}, \nexpected_cos_t: \n{expected_cos_t}"
+        assert torch.allclose(sin_t, expected_sin_t, atol=1e-3), f"sin_t: \n{sin_t}, \nexpected_sin_t: \n{expected_sin_t}"
---
+++ tests/test_shape_utils.py
@@ -0,0 +1,145 @@
+import torch
+from gtno_py.gtno.shape_utils import flatten_spatiotemporal, unflatten_spatiotemporal
+
+
+class TestShapeUtilsGrouped:
+    def test_flatten_unflatten_spatiotemporal(self):
+        batch_size = 2
+        num_timesteps = 3
+        num_nodes = 4
+        d = 5
+
+        unflattened_x = torch.tensor(
+            [
+                [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
+                [[21, 22, 23, 24, 25], [26, 27, 28, 29, 30], [31, 32, 33, 34, 35], [36, 37, 38, 39, 40]],
+                [[41, 42, 43, 44, 45], [46, 47, 48, 49, 50], [51, 52, 53, 54, 55], [56, 57, 58, 59, 60]],
+                [[61, 62, 63, 64, 65], [66, 67, 68, 69, 70], [71, 72, 73, 74, 75], [76, 77, 78, 79, 80]],
+                [[81, 82, 83, 84, 85], [86, 87, 88, 89, 90], [91, 92, 93, 94, 95], [96, 97, 98, 99, 100]],
+                [[101, 102, 103, 104, 105], [106, 107, 108, 109, 110], [111, 112, 113, 114, 115], [116, 117, 118, 119, 120]],
+            ],
+            dtype=torch.int32,
+        )
+
+        flattened_x = flatten_spatiotemporal(unflattened_x, num_timesteps)
+        unflattened_x_2 = unflatten_spatiotemporal(flattened_x, num_timesteps)
+
+        assert torch.equal(unflattened_x_2, unflattened_x)
+
+    def test_flatten_spatiotemporal(self):
+        batch_size = 2
+        num_timesteps = 3
+        num_nodes = 4
+        d = 5
+
+        # Batches = blue; Nodes = yellow
+        unflattened_x = torch.tensor(
+            [
+                [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
+                [[21, 22, 23, 24, 25], [26, 27, 28, 29, 30], [31, 32, 33, 34, 35], [36, 37, 38, 39, 40]],
+                [[41, 42, 43, 44, 45], [46, 47, 48, 49, 50], [51, 52, 53, 54, 55], [56, 57, 58, 59, 60]],
+                [[61, 62, 63, 64, 65], [66, 67, 68, 69, 70], [71, 72, 73, 74, 75], [76, 77, 78, 79, 80]],
+                [[81, 82, 83, 84, 85], [86, 87, 88, 89, 90], [91, 92, 93, 94, 95], [96, 97, 98, 99, 100]],
+                [[101, 102, 103, 104, 105], [106, 107, 108, 109, 110], [111, 112, 113, 114, 115], [116, 117, 118, 119, 120]],
+            ],
+            dtype=torch.int32,
+        )
+
+        flattened_x = flatten_spatiotemporal(unflattened_x, num_timesteps)
+
+        assert flattened_x.shape == (batch_size, num_nodes * num_timesteps, d)
+
+        # Batches = blue; Nodes * time = yellow
+        expected_flattened_x = torch.tensor(
+            [
+                [
+                    [1, 2, 3, 4, 5],
+                    [6, 7, 8, 9, 10],
+                    [11, 12, 13, 14, 15],
+                    [16, 17, 18, 19, 20],
+                    [21, 22, 23, 24, 25],
+                    [26, 27, 28, 29, 30],
+                    [31, 32, 33, 34, 35],
+                    [36, 37, 38, 39, 40],
+                    [41, 42, 43, 44, 45],
+                    [46, 47, 48, 49, 50],
+                    [51, 52, 53, 54, 55],
+                    [56, 57, 58, 59, 60],
+                ],
+                [
+                    [61, 62, 63, 64, 65],
+                    [66, 67, 68, 69, 70],
+                    [71, 72, 73, 74, 75],
+                    [76, 77, 78, 79, 80],
+                    [81, 82, 83, 84, 85],
+                    [86, 87, 88, 89, 90],
+                    [91, 92, 93, 94, 95],
+                    [96, 97, 98, 99, 100],
+                    [101, 102, 103, 104, 105],
+                    [106, 107, 108, 109, 110],
+                    [111, 112, 113, 114, 115],
+                    [116, 117, 118, 119, 120],
+                ],
+            ]
+        )
+
+        assert flattened_x.shape == expected_flattened_x.shape
+        assert torch.equal(flattened_x, expected_flattened_x), f"Flattened x: {flattened_x}\nExpected flattened x: {expected_flattened_x}"
+
+    def test_unflatten_spatiotemporal(self):
+        batch_size = 2
+        num_timesteps = 3
+        num_nodes = 4
+        d = 5
+
+        # Batches = blue; Nodes * time = yellow
+        flattened_x = torch.tensor(
+            [
+                [
+                    [1, 2, 3, 4, 5],
+                    [6, 7, 8, 9, 10],
+                    [11, 12, 13, 14, 15],
+                    [16, 17, 18, 19, 20],
+                    [21, 22, 23, 24, 25],
+                    [26, 27, 28, 29, 30],
+                    [31, 32, 33, 34, 35],
+                    [36, 37, 38, 39, 40],
+                    [41, 42, 43, 44, 45],
+                    [46, 47, 48, 49, 50],
+                    [51, 52, 53, 54, 55],
+                    [56, 57, 58, 59, 60],
+                ],
+                [
+                    [61, 62, 63, 64, 65],
+                    [66, 67, 68, 69, 70],
+                    [71, 72, 73, 74, 75],
+                    [76, 77, 78, 79, 80],
+                    [81, 82, 83, 84, 85],
+                    [86, 87, 88, 89, 90],
+                    [91, 92, 93, 94, 95],
+                    [96, 97, 98, 99, 100],
+                    [101, 102, 103, 104, 105],
+                    [106, 107, 108, 109, 110],
+                    [111, 112, 113, 114, 115],
+                    [116, 117, 118, 119, 120],
+                ],
+            ]
+        )
+
+        # Batches = blue; Nodes = yellow
+        expected_unflattened = torch.tensor(
+            [
+                [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
+                [[21, 22, 23, 24, 25], [26, 27, 28, 29, 30], [31, 32, 33, 34, 35], [36, 37, 38, 39, 40]],
+                [[41, 42, 43, 44, 45], [46, 47, 48, 49, 50], [51, 52, 53, 54, 55], [56, 57, 58, 59, 60]],
+                [[61, 62, 63, 64, 65], [66, 67, 68, 69, 70], [71, 72, 73, 74, 75], [76, 77, 78, 79, 80]],
+                [[81, 82, 83, 84, 85], [86, 87, 88, 89, 90], [91, 92, 93, 94, 95], [96, 97, 98, 99, 100]],
+                [[101, 102, 103, 104, 105], [106, 107, 108, 109, 110], [111, 112, 113, 114, 115], [116, 117, 118, 119, 120]],
+            ],
+            dtype=torch.int32,
+        )
+
+        unflattened_x = unflatten_spatiotemporal(flattened_x, num_timesteps)
+
+        assert unflattened_x.shape == expected_unflattened.shape
+        assert torch.equal(unflattened_x, expected_unflattened)
