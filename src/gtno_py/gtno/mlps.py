b2b1ebbbe00d780ec814154e8b76db75f62ca8e4
Luke Thompson <luke-a-thompson@outlook.com>
2025-01-28 20:42:03 +1100

    Feat: Dropout for MLP

--- src/gtno_py/gtno/mlps.py
+++ src/gtno_py/gtno/mlps.py
@@ -5,7 +5,7 @@ from typing import final, override
 
 @final
 class MLP(nn.Module):
-    def __init__(self, in_features: int, out_features: int, hidden_features: int, hidden_layers: int, activation: nn.Module) -> None:
+    def __init__(self, in_features: int, out_features: int, hidden_features: int, hidden_layers: int, activation: nn.Module, dropout_p: float = 0.0) -> None:
         """
         A simple MLP with a specified number of layers and hidden features.
 
@@ -15,7 +15,7 @@ class MLP(nn.Module):
             hidden_features: The number of features in the hidden layers.
             hidden_layers: The number of hidden layers.
             activation: The activation function to use in the hidden layers.
-
+            dropout_p: The dropout probability.
         num_layers = 1 produces an MLP of the form:
             Linear(in_features, hidden_features) -> activation -> Linear(hidden_features, out_features)
         """
@@ -23,7 +23,7 @@ class MLP(nn.Module):
         layers = []
         for i in range(hidden_layers):
             in_size = in_features if i == 0 else hidden_features
-            layers.extend([nn.Linear(in_size, hidden_features), activation])
+            layers.extend([nn.Linear(in_size, hidden_features), activation, nn.Dropout(dropout_p)])
         layers.append(nn.Linear(hidden_features, out_features))
         self.layers = nn.ModuleList(layers)
 
