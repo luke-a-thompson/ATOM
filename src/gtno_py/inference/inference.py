f9f48638eeafe051bc994667744ab67150c717ab
Luke Thompson <luke-a-thompson@outlook.com>
2025-05-10 17:24:40 +1000

    Fix: Allow inference on torch.compiled models

--- src/gtno_py/inference/inference.py
+++ src/gtno_py/inference/inference.py
@@ -1,4 +1,4 @@
-from gtno_py.inference.inference_utils import parse_inference_args
+from gtno_py.inference.inference_utils import parse_inference_args, clean_state_dict_prefixes
 from gtno_py.training import Config, eval_epoch, create_dataloaders_single, create_dataloaders_multitask
 import torch
 from gtno_py.training import initialize_model
@@ -13,7 +13,7 @@ def main() -> None:
         raise FileNotFoundError(f"Config file {args.config} not found")
 
     try:
-        model_state_dict: OrderedDict[str, torch.Tensor] = torch.load(args.model, weights_only=True)
+        model_state_dict: OrderedDict[str, torch.Tensor] = torch.load(str(args.model), weights_only=True)
     except FileNotFoundError:
         raise FileNotFoundError(f"Model file {args.model} not found")
 
@@ -23,7 +23,8 @@ def main() -> None:
         test_loader = create_dataloaders_single(config)[2]
 
     model = initialize_model(config).to(config.training.device)
-    _ = model.load_state_dict(model_state_dict)
+    clean_model_state_dict = clean_state_dict_prefixes(model_state_dict)
+    _ = model.load_state_dict(clean_model_state_dict)
     _ = model.eval()
 
     test_s2t_loss, test_s2s_loss = eval_epoch(config, model, test_loader)
