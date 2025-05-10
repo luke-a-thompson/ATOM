de0f6f76c3bad595a9dd7b48b0e20024ae67438e
Luke Thompson <luke-a-thompson@outlook.com>
2025-05-10 17:25:02 +1000

    Feat: Func to remove torch.compile state dict artefacts

--- src/gtno_py/inference/inference_utils.py
+++ src/gtno_py/inference/inference_utils.py
@@ -1,4 +1,6 @@
 import argparse
+from collections import OrderedDict
+import torch
 
 
 def parse_inference_args() -> argparse.Namespace:
@@ -14,3 +16,18 @@ def parse_inference_args() -> argparse.Namespace:
         help="Path to a config.toml file",
     )
     return parser.parse_args()
+
+
+def clean_state_dict_prefixes(state_dict: OrderedDict[str, torch.Tensor]) -> OrderedDict[str, torch.Tensor]:
+    """
+    Remove the '_orig_mod.' prefix from the state_dict keys that is added by torch.compile.
+    """
+    new_state_dict: OrderedDict[str, torch.Tensor] = OrderedDict()
+    for k, v in state_dict.items():
+        if k.startswith("_orig_mod."):
+            new_key = k[len("_orig_mod.") :]
+        else:
+            new_key = k
+        new_state_dict[new_key] = v
+
+    return new_state_dict
